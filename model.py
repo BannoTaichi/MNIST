26dee87c308f22337d878d81db0b161a7745969f
Banno Taichi <taichib@icloud.com>
2024-10-20 23:42:34 +0900

    Create Model Architecture

---
+++ model.py
@@ -0,0 +1,17 @@
+# conda create -n mnist python=3.10
+# conda install tensorflow
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train.reshape(-1, 784) / 255.0, x_test.reshape(-1, 784) / 255.0
+model = tf.keras.models.Sequential(
+    [
+        tf.keras.layers.Dense(128, activation="relu", input_shape=(784,)),
+        tf.keras.layers.Dense(10, activation="softmax"),
+    ]
+)
+model.compile(
+    optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+)
+model.summary()
